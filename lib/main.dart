962319ee47f0e213f00409ca8768ce83506b423d
Alokkumar2003-Coder <alokkumar2000.5.5@gmail.com>
2024-10-13 23:01:27 +0530

    8th Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:my_portfolio/firebase_options.dart';
 import 'package:my_portfolio/pages/home_page.dart';
 import 'package:my_portfolio/styles/theme.dart';
 
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,);
-  runApp(const MyApp());
-}
-
+void main() {}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
