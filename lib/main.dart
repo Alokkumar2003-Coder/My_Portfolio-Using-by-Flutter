697e430aec7d0e56bf0377fb7aa0f3a53dfa736d
Alokkumar2003-Coder <alokkumar2000.5.5@gmail.com>
2024-10-13 23:10:52 +0530

    9th Commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:my_portfolio/pages/home_page.dart';
 import 'package:my_portfolio/styles/theme.dart';
 
-void main() {}
+void main() {
+  runApp(const MyApp());
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
